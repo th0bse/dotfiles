b12f1a5a6b96125a625cd26fb2eb07c7c7ec2237
Thorben von Rekowski <rekowski.thorben@th0bse.com>
2021-03-31 18:37:16 +0200

    Add script to import all changes
    The script copies all files that are to be tracked in this repository,
    effectively importing all changes made.
    
    Author: Thorben von Rekowski <rekowski.thorben@th0bse.com>

---
+++ update-files.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Copy all relevant files into the repository, so all changes get
+# imported into the repo.
+cp ~/.gitconfig ./general/
+cp ~/.config/gitcommittemplate ./general/
+cp ~/.bashrc ./tvrarch/
+cp ~/.vimrc ./tvrarch/
+cp ~/.zshrc ./tvrarch/
