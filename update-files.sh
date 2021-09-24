9212a903e0821b69b72273fbc182846f617a48df
Thorben von Rekowski <thorben.von-rekowski@gwdg.de>
2021-09-24 10:02:27 +0200

    refactor!:remove update script
    The update script is old, and not works properly anymore.
    
    Author: Thorben von Rekowski <thorben.von-rekowski@gwdg.de>

--- update-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# Copy all relevant files into the repository, so all changes get
-# imported into the repo.
-cp ~/.gitconfig ./general/
-cp ~/.config/gitcommittemplate ./general/
-cp ~/.bashrc ./archtvr/
-cp ~/.vimrc ./archtvr/
-cp ~/.zshrc ./archtvr/
