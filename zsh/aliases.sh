3da0fc2d45843e8cc549e4eaae02eb4f2aa52ae0
Thorben von Rekowski <thorben.von-rekowski@gwdg.de>
2023-01-09 16:50:22 +0100

    feat(zsh): add missing files
    * Add aliases.sh, where all user-defined aliases reside
    * Add the userfunctions folder, where all userfunctions reside

---
+++ zsh/aliases.sh
@@ -0,0 +1,16 @@
+#! /bin/zsh
+# Config file for configuring commonly used, user defined, aliases.
+# This file gets sourced in the .zshrc
+
+# always use nvim
+alias vim=nvim
+
+# cls should also clear the terminal. It's shorter why not use it
+alias cls=clear
+
+# various aliases for zeusctl
+alias zctl='start_gwdg_wg; ~/zeusctl/zeusdog.sh'
+alias zctlp='start_gwdg_wg; zctl punch'
+alias zctll='start_gwdg_wg; zctl lunch'
+alias zctls='start_gwdg_wg; zctl status'
+alias zctlr='start_gwdg_wg; zctl report'
