0cd142cead6c8914cde31b618b23aef3ba1565e5
Thorben von Rekowski <thorben.von-rekowski@gwdg.de>
2024-07-19 12:21:01 +0200

    refactor(zsh): remove work related stuff

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,11 +7,3 @@ alias vim=nvim
 
 # cls should also clear the terminal. It's shorter why not use it
 alias cls=clear
-
-# various aliases for zeusctl
-alias zctl='~/scripts/zeusctl/zeusctl.pl'
-alias zctlp='zctl punch'
-alias zctll='zctl lunch'
-alias zctls='zctl status'
-alias zctlr='zctl report'
-alias zctle='colleague_status'
